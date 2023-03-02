add4227c569a3b63f57311735681511240fb7412
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-03-02 17:14:01 +0100

    definizione rotte per edit e update di Photo NO Validation

--- src/main/java/org/lessons/java/fotoalbum/controller/PhotoController.java
+++ src/main/java/org/lessons/java/fotoalbum/controller/PhotoController.java
@@ -3,12 +3,17 @@ package org.lessons.java.fotoalbum.controller;
 import java.util.List;
 
 import org.lessons.java.fotoalbum.model.Photo;
+import org.lessons.java.fotoalbum.model.Tag;
 import org.lessons.java.fotoalbum.repository.PhotoRepository;
+import org.lessons.java.fotoalbum.repository.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,11 +21,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PhotoController {
 
 	@Autowired
-	private PhotoRepository photosRepo;
+	private PhotoRepository photoRepo;
+
+	@Autowired
+	private TagRepository tagRepo;
 
 	@GetMapping
 	public String index(Model model) {
-		List<Photo> photos = photosRepo.findAll();
+		List<Photo> photos = photoRepo.findAll();
 		model.addAttribute("photos", photos);
 
 		return "photos/index";
@@ -28,10 +36,36 @@ public class PhotoController {
 
 	@GetMapping("{id}")
 	public String show(@PathVariable(name = "id") long id, Model model) {
-		Photo photo = photosRepo.getReferenceById(id);
+		Photo photo = photoRepo.getReferenceById(id);
 		model.addAttribute("photo", photo);
 
 		return "photos/show";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") long id, Model model) {
+		Photo photo = photoRepo.getReferenceById(id);
+		List<Tag> tags = tagRepo.findAllByOrderByName();
+
+		model.addAttribute("photo", photo);
+		model.addAttribute("tagsList", tags);
+
+		return "photos/edit";
+	}
+
+	@PostMapping("/edit/{id}")
+	public String update(
+//			@Valid 
+			@ModelAttribute("photo") Photo formPhoto, BindingResult bindingResult, Model model) {
+
+//		if (bindingResult.hasErrors()) {
+//			model.addAttribute("tagsList", tagRepo.findAllByOrderByName());
+//			return "photos/edit";
+//		}
+
+		photoRepo.save(formPhoto);
+
+		return "redirect:/photos";
 	}
 
 }
