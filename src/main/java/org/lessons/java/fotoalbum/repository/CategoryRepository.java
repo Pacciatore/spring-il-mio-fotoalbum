a4434186a897961b6c92133fbd51f2586ff54330
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-03-02 17:10:09 +0100

    aggiunto ordinamento per nome

--- src/main/java/org/lessons/java/fotoalbum/repository/CategoryRepository.java
+++ src/main/java/org/lessons/java/fotoalbum/repository/CategoryRepository.java
@@ -1,8 +1,12 @@
 package org.lessons.java.fotoalbum.repository;
 
+import java.util.List;
+
 import org.lessons.java.fotoalbum.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+	public List<Category> findAllByOrderByName();
 
 }
--- src/main/java/org/lessons/java/fotoalbum/repository/TagRepository.java
+++ src/main/java/org/lessons/java/fotoalbum/repository/TagRepository.java
@@ -1,8 +1,12 @@
 package org.lessons.java.fotoalbum.repository;
 
+import java.util.List;
+
 import org.lessons.java.fotoalbum.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+	public List<Tag> findAllByOrderByName();
 
 }
