b2cad8adccc42010eb43556b0bf8508dc7c6c06b
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-03-02 13:26:17 +0100

    aggiunto index con restituzione di tutte le foto in db

---
+++ src/main/java/org/lessons/java/fotoalbum/controller/PhotoController.java
@@ -0,0 +1,28 @@
+package org.lessons.java.fotoalbum.controller;
+
+import java.util.List;
+
+import org.lessons.java.fotoalbum.model.Photo;
+import org.lessons.java.fotoalbum.repository.PhotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/photos")
+public class PhotoController {
+
+	@Autowired
+	private PhotoRepository photosRepo;
+
+	@GetMapping
+	public String index(Model model) {
+		List<Photo> photos = photosRepo.findAll();
+		model.addAttribute("photos", photos);
+
+		return "photos/index";
+	}
+
+}
