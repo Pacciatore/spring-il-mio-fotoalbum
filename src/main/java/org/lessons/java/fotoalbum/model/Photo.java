ebf8de78eb88f43b381d861966c0053a366503ce
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-03-02 11:57:39 +0100

    creazione model e repository di Photo

---
+++ src/main/java/org/lessons/java/fotoalbum/model/Photo.java
@@ -0,0 +1,70 @@
+package org.lessons.java.fotoalbum.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "photos")
+public class Photo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String title;
+
+	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public boolean isVisible() {
+		return visible;
+	}
+
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+
+	private String url;
+
+	private boolean visible;
+
+//	 TODO: Modelli per Tag e Categories + Relazioni
+//	 private List<Tag> tags;
+//	 
+//	 private List<Category> categories;
+
+}
