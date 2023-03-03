5e280b187f190b05771a2916985109e56ba43e7a
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-03-03 18:36:43 +0100

    definizione rotta front per il dettaglio delle Photo

--- src/main/java/org/lessons/java/fotoalbum/controller/FrontController.java
+++ src/main/java/org/lessons/java/fotoalbum/controller/FrontController.java
@@ -3,6 +3,7 @@ package org.lessons.java.fotoalbum.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -11,6 +12,11 @@ public class FrontController {
 	@GetMapping()
 	public String index() {
 		return "front/homepage";
+	}
+
+	@GetMapping("/photo")
+	public String show(@RequestParam(name = "photo", required = false) String keyword) {
+		return "front/show";
 	}
 
 }
