1f02bcae4ed742a0806b1bc72e9a242aa24370a6
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-03-03 12:26:39 +0100

    implementazione show CategoryController

--- src/main/java/org/lessons/java/fotoalbum/controller/CategoryController.java
+++ src/main/java/org/lessons/java/fotoalbum/controller/CategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,6 +24,14 @@ public class CategoryController {
 		model.addAttribute("categories", categories);
 
 		return "categories/index";
+	}
+
+	@GetMapping("{id}")
+	public String show(@PathVariable("id") long id, Model model) {
+		Category category = categoryRepo.getReferenceById(id);
+		model.addAttribute("category", category);
+
+		return "categories/show";
 	}
 
 }
