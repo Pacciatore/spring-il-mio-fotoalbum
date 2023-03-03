edee67e82e7e0e8b6da55c633fa06580bd9112a0
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-03-03 15:57:11 +0100

    Creazione e implementazione ApiController

---
+++ src/main/java/org/lessons/java/fotoalbum/api/PhotoApiController.java
@@ -0,0 +1,69 @@
+package org.lessons.java.fotoalbum.api;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.lessons.java.fotoalbum.model.Photo;
+import org.lessons.java.fotoalbum.repository.PhotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/photos")
+public class PhotoApiController {
+
+	@Autowired
+	private PhotoRepository photoRepo;
+
+	@GetMapping
+	public List<Photo> index(@RequestParam(name = "photo", required = false) String keyword) {
+//		if (keyword == null || keyword.isEmpty())
+		return photoRepo.findAll();
+//		else
+//			return photoRepo.findByNameLike("%" + keyword + "%");
+	}
+
+	@GetMapping("{id}")
+	public ResponseEntity<Photo> show(@PathVariable(name = "id") long id) {
+		Optional<Photo> result = photoRepo.findById(id);
+
+		if (result.isPresent())
+			return new ResponseEntity<Photo>(result.get(), HttpStatus.OK);
+		else
+			return new ResponseEntity<Photo>(HttpStatus.NOT_FOUND);
+	}
+
+	@PostMapping()
+	public Photo create(@RequestBody Photo photo) {
+		return photoRepo.save(photo);
+	}
+
+	@PutMapping("{id}")
+	public Photo edit(@RequestBody Photo photo, @PathVariable(name = "id") long id) {
+		Photo editedPhoto = photoRepo.getReferenceById(id);
+
+		editedPhoto.setTitle(photo.getTitle());
+		editedPhoto.setDescription(photo.getDescription());
+		editedPhoto.setUrl(photo.getUrl());
+		editedPhoto.setVisible(photo.isVisible());
+
+		return photoRepo.save(editedPhoto);
+	}
+
+	@DeleteMapping("{id}")
+	public void delete(@PathVariable(name = "id") long id) {
+		photoRepo.deleteById(id);
+	}
+}
