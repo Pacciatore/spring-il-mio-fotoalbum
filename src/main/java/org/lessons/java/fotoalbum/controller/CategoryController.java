74f4aa79530e7ba8534dc31f0acedc2dfd47080a
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-03-03 12:12:13 +0100

    creazione CategoryController + index

---
+++ src/main/java/org/lessons/java/fotoalbum/controller/CategoryController.java
@@ -0,0 +1,28 @@
+package org.lessons.java.fotoalbum.controller;
+
+import java.util.List;
+
+import org.lessons.java.fotoalbum.model.Category;
+import org.lessons.java.fotoalbum.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+
+	@Autowired
+	private CategoryRepository categoryRepo;
+
+	@GetMapping()
+	public String index(Model model) {
+		List<Category> categories = categoryRepo.findAllByOrderByName();
+		model.addAttribute("categories", categories);
+
+		return "categories/index";
+	}
+
+}
