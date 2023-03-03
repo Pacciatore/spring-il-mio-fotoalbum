2f767bd2297c0ea5e2fcd048b434ddc8982be7b2
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-03-03 14:34:33 +0100

    implementazione show TagController

--- src/main/java/org/lessons/java/fotoalbum/controller/TagController.java
+++ src/main/java/org/lessons/java/fotoalbum/controller/TagController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,6 +24,14 @@ public class TagController {
 		model.addAttribute("tags", tags);
 
 		return "tags/index";
+	}
+
+	@GetMapping("{id}")
+	public String show(@PathVariable("id") long id, Model model) {
+		Tag tag = tagRepo.getReferenceById(id);
+		model.addAttribute("tag", tag);
+
+		return "tags/show";
 	}
 
 }
