48f7a0b1c7ad952bfc50648256d8dbfcf3342db3
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-03-03 10:17:18 +0100

    aggiunti tag e commenti in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -19,3 +19,18 @@ INSERT INTO spring_fotoalbum.categories(name) VALUES('Lavoro');
 INSERT INTO spring_fotoalbum.categories(name) VALUES('Sport');
 INSERT INTO spring_fotoalbum.categories(name) VALUES('Cucina');
 INSERT INTO spring_fotoalbum.categories(name) VALUES('Informatica');
+
+INSERT INTO spring_fotoalbum.comments(content, photo_id) VALUES('Bella foto!', 1);
+INSERT INTO spring_fotoalbum.comments(content, photo_id) VALUES('Mi ricordo di questo giorno', 1);
+INSERT INTO spring_fotoalbum.comments(content, photo_id) VALUES('Bella foto!', 5);
+INSERT INTO spring_fotoalbum.comments(content, photo_id) VALUES('Bella foto!', 6);
+INSERT INTO spring_fotoalbum.comments(content, photo_id) VALUES('Bella foto!', 4);
+INSERT INTO spring_fotoalbum.comments(content, photo_id) VALUES('Bella foto!', 7);
+INSERT INTO spring_fotoalbum.comments(content, photo_id) VALUES('Bella foto!', 7);
+INSERT INTO spring_fotoalbum.comments(content, photo_id) VALUES('Non ti ricordavo così!', 7);
+
+INSERT INTO spring_fotoalbum.tags(name) VALUES('Amicizia');
+INSERT INTO spring_fotoalbum.tags(name) VALUES('Divertimento');
+INSERT INTO spring_fotoalbum.tags(name) VALUES('Passione');
+INSERT INTO spring_fotoalbum.tags(name) VALUES('Tristezza');
+INSERT INTO spring_fotoalbum.tags(name) VALUES('Rabbia');
