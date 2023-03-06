2418c8c4cd6dd89078842fbaafbe07e5ab8305bb
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-03-06 10:50:07 +0100

    filtri funzionanti, non in maniera ottimale

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -9,7 +9,7 @@ searchBar.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         event.preventDefault();
         console.log("Ricerca", searchBar.value);
-        photosListQuery(searchBar.value);
+        photosListQuery(searchBar.value, 'searchBar');
     }
 });
 
@@ -94,85 +94,250 @@ function photosList() {
         })
 }
 
-function photosListQuery(queryParam) {
+function photosListQuery(queryParam, filterType) {
     let loading = true;
     document.getElementById('photosContainer').innerHTML = `
         <div class="spinner-border text-light mt-5" role="status">
              <span class="visually-hidden">Loading...</span>
         </div>`;
 
-    axios.get(`http://localhost:8080/api/photos?photo=${queryParam}`)
-        .then((result) => {
-            loading = false;
+    if (filterType === 'searchBar') {
+        axios.get(`http://localhost:8080/api/photos?photo=${queryParam}`)
+            .then((result) => {
+                loading = false;
 
-            document.getElementById('photosContainer').innerHTML = "";
+                document.getElementById('photosContainer').innerHTML = "";
 
-            const photosList = result.data;
-            console.log("Elenco foto", photosList);
+                const photosList = result.data;
+                console.log("Elenco foto", photosList);
 
-            photosList.forEach(photo => {
+                photosList.forEach(photo => {
 
-                if (!photo.visible) {
-                    return;
-                }
+                    if (!photo.visible) {
+                        return;
+                    }
 
-                const tags = photo.tags;
-                const categories = photo.categories;
+                    const tags = photo.tags;
+                    const categories = photo.categories;
 
-                document.getElementById('photosContainer').innerHTML += `
-                    <div class="card position-relative" style="min-height: 200px; min-width: 200px">
+                    document.getElementById('photosContainer').innerHTML += `
+                            <div class="card position-relative" style="min-height: 200px; min-width: 200px">
+        
+                                <!-- Immagine della foto -->
+                                <img class="card-img" src="${photo.url}" alt="foto_${photo.id}">
+        
+                                <a href="./photo?id=${photo.id}" class="stretched-link"></a>
+        
+                                <!-- Contenuto della card -->
+                                <div class="card-img-overlay">
+                                    <!-- Titolo della foto -->
+                                    <h5 class="card-title">${photo.title}</h5>
+            
+                                    <!-- Descrizione della foto -->
+                                    <p class="card-text text-truncate">${photo.description}</p>
+            
+                                    <!-- Se commenti non è vuoto -->
+                                    <p class="text-secondary m-0">Commenti: ${photo.comments.length}</p>
+            
+                                    <!-- Se tag non è vuoto -->
+                                    <ul id="tagsList_${photo.id}" class="list-unstyled d-flex flex-wrap gap-1 text-warning mb-0">
+                                        
+                                    </ul>
+            
+                                    <!-- Se categorie non è vuoto -->
+                                    <ul id="categoriesList_${photo.id}" class="list-unstyled d-flex flex-wrap gap-1 text-info mb-0">
+                                        
+                                    </ul>
+        
+                                </div>  
+        
+                            </div>`;
 
-                        <!-- Immagine della foto -->
-                        <img class="card-img" src="${photo.url}" alt="foto_${photo.id}">
+                    tags.forEach(tag => {
+                        document.getElementById(`tagsList_${photo.id}`).innerHTML += `
+                            <li>${tag.name}</li>
+                            `;
+                    })
 
-                        <a href="./photo?id=${photo.id}" class="stretched-link"></a>
-
-                        <!-- Contenuto della card -->
-                        <div class="card-img-overlay">
-                            <!-- Titolo della foto -->
-                            <h5 class="card-title">${photo.title}</h5>
-    
-                            <!-- Descrizione della foto -->
-                            <p class="card-text text-truncate">${photo.description}</p>
-    
-                            <!-- Se commenti non è vuoto -->
-                            <p class="text-secondary m-0">Commenti: ${photo.comments.length}</p>
-    
-                            <!-- Se tag non è vuoto -->
-                            <ul id="tagsList_${photo.id}" class="list-unstyled d-flex flex-wrap gap-1 text-warning mb-0">
-                                
-                            </ul>
-    
-                            <!-- Se categorie non è vuoto -->
-                            <ul id="categoriesList_${photo.id}" class="list-unstyled d-flex flex-wrap gap-1 text-info mb-0">
-                                
-                            </ul>
-
-                        </div>  
-
-                    </div>`;
-
-                tags.forEach(tag => {
-                    document.getElementById(`tagsList_${photo.id}`).innerHTML += `
-                    <li>${tag.name}</li>
-                    `;
-                })
-
-                categories.forEach(category => {
-                    document.getElementById(`categoriesList_${photo.id}`).innerHTML += `
-                    <li>${category.name}</li>
-                    `;
-                })
+                    categories.forEach(category => {
+                        document.getElementById(`categoriesList_${photo.id}`).innerHTML += `
+                            <li>${category.name}</li>
+                            `;
+                    })
 
 
-            });
+                });
 
-        }).catch((result) => {
-            loading = false;
+            }).catch((result) => {
+                loading = false;
 
-            console.error("Errore nella richiesta", result);
-            alert("Errore durante la richiesta!")
-        })
+                console.error("Errore nella richiesta", result);
+                alert("Errore durante la richiesta!")
+            })
+    } else if (filterType === 'tags') {
+        console.log('ricerca per tags');
+        axios.get(`http://localhost:8080/api/photos`)
+            .then((result) => {
+                loading = false;
+
+                document.getElementById('photosContainer').innerHTML = "";
+
+                const photosList = result.data;
+                console.log("Elenco foto", photosList);
+
+                photosList.forEach(photo => {
+
+                    if (!photo.visible) {
+                        return;
+                    }
+
+                    const tags = photo.tags;
+                    const categories = photo.categories;
+
+                    tags.forEach(tag => {
+
+                        if (tag.id == Number.parseInt(queryParam.id)) {
+
+                            document.getElementById('photosContainer').innerHTML += `
+                                <div class="card position-relative" style="min-height: 200px; min-width: 200px">
+            
+                                    <!-- Immagine della foto -->
+                                    <img class="card-img" src="${photo.url}" alt="foto_${photo.id}">
+            
+                                    <a href="./photo?id=${photo.id}" class="stretched-link"></a>
+            
+                                    <!-- Contenuto della card -->
+                                    <div class="card-img-overlay">
+                                        <!-- Titolo della foto -->
+                                        <h5 class="card-title">${photo.title}</h5>
+                
+                                        <!-- Descrizione della foto -->
+                                        <p class="card-text text-truncate">${photo.description}</p>
+                
+                                        <!-- Se commenti non è vuoto -->
+                                        <p class="text-secondary m-0">Commenti: ${photo.comments.length}</p>
+                
+                                        <!-- Se tag non è vuoto -->
+                                        <ul id="tagsList_${photo.id}" class="list-unstyled d-flex flex-wrap gap-1 text-warning mb-0">
+                                            
+                                        </ul>
+                
+                                        <!-- Se categorie non è vuoto -->
+                                        <ul id="categoriesList_${photo.id}" class="list-unstyled d-flex flex-wrap gap-1 text-info mb-0">
+                                            
+                                        </ul>
+            
+                                    </div>  
+            
+                                </div>`;
+
+                            tags.forEach(tag => {
+                                document.getElementById(`tagsList_${photo.id}`).innerHTML += `
+                                <li>${tag.name}</li>
+                                `;
+                            })
+
+                            categories.forEach(category => {
+                                document.getElementById(`categoriesList_${photo.id}`).innerHTML += `
+                                <li>${category.name}</li>
+                                `;
+                            })
+
+                        }
+                    });
+
+                });
+
+            }).catch((result) => {
+                loading = false;
+
+                console.error("Errore nella richiesta", result);
+                alert("Errore durante la richiesta!")
+            })
+    } else if (filterType === 'categories') {
+        console.log('ricerca per categories');
+
+        axios.get(`http://localhost:8080/api/photos`)
+            .then((result) => {
+                loading = false;
+
+                document.getElementById('photosContainer').innerHTML = "";
+
+                const photosList = result.data;
+                console.log("Elenco foto", photosList);
+
+                photosList.forEach(photo => {
+
+                    if (!photo.visible) {
+                        return;
+                    }
+
+                    const tags = photo.tags;
+                    const categories = photo.categories;
+
+                    categories.forEach(category => {
+
+                        if (category.id == Number.parseInt(queryParam.id)) {
+
+                            document.getElementById('photosContainer').innerHTML += `
+                            <div class="card position-relative" style="min-height: 200px; min-width: 200px">
+        
+                                <!-- Immagine della foto -->
+                                <img class="card-img" src="${photo.url}" alt="foto_${photo.id}">
+        
+                                <a href="./photo?id=${photo.id}" class="stretched-link"></a>
+        
+                                <!-- Contenuto della card -->
+                                <div class="card-img-overlay">
+                                    <!-- Titolo della foto -->
+                                    <h5 class="card-title">${photo.title}</h5>
+            
+                                    <!-- Descrizione della foto -->
+                                    <p class="card-text text-truncate">${photo.description}</p>
+            
+                                    <!-- Se commenti non è vuoto -->
+                                    <p class="text-secondary m-0">Commenti: ${photo.comments.length}</p>
+            
+                                    <!-- Se tag non è vuoto -->
+                                    <ul id="tagsList_${photo.id}" class="list-unstyled d-flex flex-wrap gap-1 text-warning mb-0">
+                                        
+                                    </ul>
+            
+                                    <!-- Se categorie non è vuoto -->
+                                    <ul id="categoriesList_${photo.id}" class="list-unstyled d-flex flex-wrap gap-1 text-info mb-0">
+                                        
+                                    </ul>
+        
+                                </div>  
+        
+                            </div>`;
+
+                            tags.forEach(tag => {
+                                document.getElementById(`tagsList_${photo.id}`).innerHTML += `
+                            <li>${tag.name}</li>
+                            `;
+                            })
+
+                            categories.forEach(category => {
+                                document.getElementById(`categoriesList_${photo.id}`).innerHTML += `
+                            <li>${category.name}</li>
+                            `;
+                            })
+
+                        }
+                    });
+
+                });
+
+            }).catch((result) => {
+                loading = false;
+
+                console.error("Errore nella richiesta", result);
+                alert("Errore durante la richiesta!")
+            })
+
+    }
+
 }
 
 function filtersList() {
@@ -195,9 +360,19 @@ function filtersList() {
 
                     tag.addEventListener('change', (event) => {
                         if (event.target.checked) {
-                            console.log('checkbox selezionata', event.target.id)
+
+                            console.log('checkbox selezionata', event.target.id);
+
+                            const tagId = event.target.value;
+                            const tagName = event.target.nextElementSibling.innerHTML;
+
+                            const tagElement = { id: tagId, name: tagName };
+                            console.log('Filtro per tag', tagElement);
+
+                            photosListQuery(tagElement, 'tags');
                         } else {
-                            console.log('checkbox deselezionata', event.target.id)
+                            console.log('checkbox deselezionata', event.target.id);
+                            photosList();
                         }
                     })
                 })
@@ -229,8 +404,17 @@ function filtersList() {
                     category.addEventListener('change', (event) => {
                         if (event.target.checked) {
                             console.log('checkbox selezionata', event.target.id)
+
+                            const categoryId = event.target.value;
+                            const categoryName = event.target.nextElementSibling.innerHTML;
+
+                            const categoryElement = { id: categoryId, name: categoryName };
+                            console.log('Filtro per tag', categoryElement);
+
+                            photosListQuery(categoryElement, 'categories');
                         } else {
-                            console.log('checkbox deselezionata', event.target.id)
+                            console.log('checkbox deselezionata', event.target.id);
+                            photosList();
                         }
                     })
                 })
